b7a379c44c572e414a3151c020dbbd465f61cc32
vadash <vadash@gmail.com>
2020-04-22 10:57:53 +0300

    Changed logic to keep cheap uniques

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Net;
 using Newtonsoft.Json.Linq;
 
@@ -18,10 +19,10 @@ namespace GetExpensiveUniques
         };
 
         private const float cutoff = 10f;
-        
+
         public static void Main(string[] args)
         {
-            var result = "";
+            var result = new HashSet<string>();
 
             foreach (var url in uniquesUrls)
             {
@@ -31,22 +32,28 @@ namespace GetExpensiveUniques
                     var o = JObject.Parse(json);
                     foreach (var line in o?["lines"])
                     {
-                        if (int.TryParse((string)line?["links"], out var links) &&
+                        if (int.TryParse((string) line?["links"], out var links) &&
                             links == 6)
                         {
                             continue;
                         }
-                        
-                        if (float.TryParse((string)line?["chaosValue"], out var chaosValue) &&
-                            chaosValue >= cutoff)
+
+                        if (float.TryParse((string) line?["chaosValue"], out var chaosValue) &&
+                            chaosValue <= cutoff)
                         {
-                            result += "\"" + (string)line?["name"] + "\"," + Environment.NewLine;
+                            result.Add((string) line?["name"]);
                         }
                     }
-                }                
+                }
             }
-            
-            File.WriteAllText("expensive.txt", result);
+
+            var result2 = result.ToList();
+            result2.Sort();
+            var result3 = result2.Aggregate("",
+                (current, line) => current + "\"" + line + "\"," + Environment.NewLine);
+
+
+            File.WriteAllText("cheap.txt", result3);
         }
     }
 }
